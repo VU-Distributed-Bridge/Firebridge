3ce40cb49eb03d8a44e8a31774a6d83fb75cd535
leroy <leroytruong@gmail.com>
2020-04-27 10:50:09 +0200

    Add initial HTTP function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
+'use strict';
+
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+const store = admin.firestore()
+
+exports.helloWorld = functions
+  .region('europe-west1')
+  .https.onRequest((request, response) => {
+    console.log(request.body);
+
+    response.send("Hello from Firebase!");
+  });
