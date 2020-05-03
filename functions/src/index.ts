de8a89a138fe53873acd0840d06adc1918497ac1
leroy <leroytruong@gmail.com>
2020-05-04 00:09:28 +0200

    Regenerate Cloud Functions as a TypeScript project

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+
+// noinspection JSUnusedGlobalSymbols
+export const helloWorld = functions
+    .region('europe-west1')
+    .https.onRequest((request, response) => {
+        response.send("Hello from Firebase!");
+    });
