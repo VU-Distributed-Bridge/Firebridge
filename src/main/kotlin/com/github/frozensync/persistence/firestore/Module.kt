c4b243f66f8f5c4c88b7e70f51a4a789141673de
leroy <leroytruong@gmail.com>
2020-05-06 03:20:22 +0200

    Refactor Firebase initialization

--- src/main/kotlin/com/github/frozensync/persistence/firestore/Module.kt
+++ src/main/kotlin/com/github/frozensync/persistence/firestore/Module.kt
@@ -1,7 +1,24 @@
 package com.github.frozensync.persistence.firestore
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.cloud.FirestoreClient
 import org.koin.dsl.module
 
 val firestoreModule = module {
-    single { FirestoreFactory.get() }
+    single {
+        initFirebase()
+        FirestoreClient.getFirestore()
+    }
 }
+
+private fun initFirebase() =
+    Thread.currentThread().contextClassLoader.getResourceAsStream("firestore-service-account-key.json").use {
+        val credentials = GoogleCredentials.fromStream(it)
+        val options = FirebaseOptions.Builder()
+            .setCredentials(credentials)
+            .setDatabaseUrl("https://bridge-store.firebaseio.com")
+            .build()
+        FirebaseApp.initializeApp(options)
+    }
