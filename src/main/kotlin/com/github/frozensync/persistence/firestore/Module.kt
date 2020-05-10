acfe05491c2e8527155593b4e16cad313afdf7a3
leroy <leroytruong@gmail.com>
2020-05-10 22:22:22 +0200

    Refactor Firestore initialization to be consistent with Cloud Logging
    Firestore now fetches its credentials from environment variable GOOGLE_APPLICATION_CREDENTIALS, just like Cloud Logging

--- src/main/kotlin/com/github/frozensync/persistence/firestore/Module.kt
+++ src/main/kotlin/com/github/frozensync/persistence/firestore/Module.kt
@@ -1,24 +1,7 @@
 package com.github.frozensync.persistence.firestore
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import com.google.firebase.cloud.FirestoreClient
 import org.koin.dsl.module
 
 val firestoreModule = module {
-    single {
-        initFirebase()
-        FirestoreClient.getFirestore()
-    }
+    single { FirestoreFactory().get() }
 }
-
-private fun initFirebase() =
-    Thread.currentThread().contextClassLoader.getResourceAsStream("firestore-service-account-key.json").use {
-        val credentials = GoogleCredentials.fromStream(it)
-        val options = FirebaseOptions.Builder()
-            .setCredentials(credentials)
-            .setDatabaseUrl("https://bridge-store.firebaseio.com")
-            .build()
-        FirebaseApp.initializeApp(options)
-    }
