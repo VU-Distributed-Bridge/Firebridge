52d46cc77af8ef25e5145e30221b0c8a80ae44d2
leroy <leroytruong@gmail.com>
2020-05-11 20:53:28 +0200

    Asserts required properties
    The assertion gives a single point of failure. Future property access can use `!!` to ignore nullables.

--- src/main/kotlin/com/github/frozensync/Main.kt
+++ src/main/kotlin/com/github/frozensync/Main.kt
@@ -7,6 +7,7 @@ import com.github.frozensync.tournament.Score
 import com.github.frozensync.tournament.TournamentService
 import com.github.frozensync.tournament.tournamentModule
 import mu.KotlinLogging
+import org.koin.core.Koin
 import org.koin.core.context.startKoin
 import java.util.*
 import kotlin.concurrent.fixedRateTimer
@@ -20,15 +21,11 @@ fun main() {
         environmentProperties()
     }
     val koin = koinApplication.koin
+    koin.assertProperties()
 
-    val idProperty = koin.getProperty<String>("RASPBERRY_PI_ID")
-    if (idProperty == null) {
-        System.err.println("Please provide an id for the raspberry pi as an environment variable.")
-        exitProcess(1)
-    }
-    val id = UUID.fromString(idProperty)
-
+    val id = UUID.fromString(koin.getProperty<String>("RASPBERRY_PI_ID"))
     logger.info { "Started with id $id." }
+
     fixedRateTimer(name = "health-check", daemon = true, period = 20000L) { logger.info { "Health check: OK" } }
 
     val raspberryPiService = koin.get<RaspberryPiService>()
@@ -39,5 +36,19 @@ fun main() {
     while (true) {
         val score = Score(scanner.nextInt())
         tournamentService.save(score)
+    }
+}
+
+/**
+ * Asserts required properties. Terminates application if any property is missing.
+ */
+private fun Koin.assertProperties() {
+    if (getProperty<String>(RASPBERRY_PI_ID) == null) {
+        System.err.println("Missing environment variable: RASPBERRY_PI_ID")
+        exitProcess(1)
+    }
+    if (getProperty<String>(GOOGLE_APPLICATION_CREDENTIALS) == null) {
+        System.err.println("Missing environment variable: GOOGLE_APPLICATION_CREDENTIALS")
+        exitProcess(1)
     }
 }
