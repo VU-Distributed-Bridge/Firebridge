e626e2010391ffd33f9421cfe83fa4ec6c843da2
leroy <leroytruong@gmail.com>
2020-08-08 23:50:42 +0200

    refactor: apply `Dispatcher.IO` context to `Firestore#retry` for cleaner caller code

--- src/main/kotlin/com/github/frozensync/database/Firestore.kt
+++ src/main/kotlin/com/github/frozensync/database/Firestore.kt
@@ -2,34 +2,35 @@ package com.github.frozensync.database
 
 import com.google.api.gax.rpc.ApiException
 import com.google.cloud.firestore.Firestore
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger { }
 
 /**
- * Returns the result of RPC call [block] with exponential backoff on failure.
+ * Returns the result of RPC call [block], executed in a [Dispatchers.IO] context, with exponential backoff on failure.
  *
  * If [block] throws an [ApiException], retries again after [initialDelay].
  * Subsequent retries are delayed by a factor of [factor] with an upper limit of [maxDelay].
  * After [times] retries, lets the thrown exception bubble upwards.
  */
-@Suppress("unused")
 suspend fun <T> Firestore.retry(
     times: Int = Int.MAX_VALUE,
     initialDelay: Long = 500, // milliseconds
     maxDelay: Long = 30000, // milliseconds
     factor: Double = 2.0,
-    block: suspend () -> T
-): T {
+    block: suspend Firestore.() -> T
+): T = withContext(Dispatchers.IO) {
     logger.entry(times, initialDelay, maxDelay, factor)
 
     var currentDelay = initialDelay
 
     repeat(times - 1) {
         try {
-            return block().also { logger.exit(it) }
-        } catch (e: ApiException) {
+            return@withContext block().also { logger.exit(it) }
+        } catch (e: Exception) {
             logger.catching(e)
         }
 
@@ -39,5 +40,10 @@ suspend fun <T> Firestore.retry(
         logger.warn { "Firestore request failed. Retrying in ${currentDelay / 1000} seconds. Attempt: ${it + 1}/$times." }
     }
 
-    return block().also { logger.exit(it) }
+    try {
+        return@withContext block().also { logger.exit(it) }
+    } catch (e: Exception) {
+        logger.catching(e)
+        throw e
+    }
 }
