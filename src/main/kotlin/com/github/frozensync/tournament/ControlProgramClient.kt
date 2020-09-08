d6c12162a9ae87f7e7bc8d967fba50fb96e86f6c
leroy <leroytruong@gmail.com>
2020-09-08 14:25:48 +0200

    improvement: expand domain model for scores (#48)

--- src/main/kotlin/com/github/frozensync/tournament/ControlProgramClient.kt
+++ src/main/kotlin/com/github/frozensync/tournament/ControlProgramClient.kt
@@ -11,7 +11,15 @@ class ControlProgramClient constructor(private val channel: ManagedChannel) : Cl
     private val stub: ControlProgramCoroutineStub = ControlProgramCoroutineStub(channel)
 
     suspend fun sendScore(score: Score) = coroutineScope {
-        val request = ScoreRequest.newBuilder().setScore(score.result).build()
+        val request = ScoreRequest.newBuilder()
+            .setRound(score.round)
+            .setNS(score.NS)
+            .setEW(score.EW)
+            .setBoard(score.board)
+            .setContr(score.contr)
+            .setLead(score.lead)
+            .setResult(score.result)
+            .build()
         stub.sendScore(request)
     }
 
--- src/main/kotlin/com/github/frozensync/tournament/Score.kt
+++ src/main/kotlin/com/github/frozensync/tournament/Score.kt
@@ -2,5 +2,11 @@ package com.github.frozensync.tournament
 
 // other fields left out for now for easier testing
 data class Score(
+    val round: Long,
+    val NS: Long, // north-south player pair
+    val EW: Long, // east-west player pair
+    val board: Long,
+    val contr: String,
+    val lead: String,
     val result: Long
 )
